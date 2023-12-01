c4394ddaacbee326f2997a94c60574817e7fdcba
turtton <top.gear7509@turtton.net>
2023-12-01 21:28:00 +0900

    :hammer: DatabaseConnection trait applied only query impl

--- kernel/src/database.rs
+++ kernel/src/database.rs
@@ -1,19 +1,19 @@
 use error_stack::{Context, Report};
 
 #[async_trait::async_trait]
-pub trait DatabaseConnection<Connection>: 'static + Sync + Send {
+pub trait QueryDatabaseConnection<Connection>: 'static + Sync + Send {
     type Error: Context;
     async fn transact(&self) -> Result<Connection, Report<Self::Error>>;
 }
 
 pub trait DependOnDatabaseConnection<Connection>: 'static + Sync + Send {
-    type DatabaseConnection: DatabaseConnection<Connection>;
+    type DatabaseConnection: QueryDatabaseConnection<Connection>;
     fn database_connection(&self) -> &Self::DatabaseConnection;
 }
 
 impl<T, C> DependOnDatabaseConnection<C> for T
 where
-    T: DatabaseConnection<C>,
+    T: QueryDatabaseConnection<C>,
 {
     type DatabaseConnection = T;
     fn database_connection(&self) -> &Self::DatabaseConnection {
